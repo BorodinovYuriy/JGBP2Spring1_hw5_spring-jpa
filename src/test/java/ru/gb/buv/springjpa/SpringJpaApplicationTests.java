42b6e497d86b05803693cb3829df7448f825d3fe
aditone <aditone2378@gmail.com>
2022-11-18 19:56:58 +0300

    дз 5

---
+++ src/test/java/ru/gb/buv/springjpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.buv.springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
